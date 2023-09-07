f05f09ddba7dc7c6b29ee328fbdfb15a4e9ee8f9
Vitor <victor133br@gmail.com>
2023-09-06 22:50:51 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,25 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, TouchableOpacity, StyleSheet, Text, View, Image } from 'react-native';
 import { Entypo } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
       <View style={sidebar.container}>
-        <Text style={sidebar.title}>WhatsApp 2</Text>
+        <Text style={sidebar.title}>Communital</Text>
         <View style={sidebar.list}>
-          <Ionicons name="search-sharp" size={20} color="white" />
-          <Entypo name="dots-three-vertical" size={20} color="white" />
+          <Ionicons name='search-sharp' size={20} color='white' />
+          <Entypo name='dots-three-vertical' size={20} color='white' />
         </View>
         <View style={sidebar2.container}>
           <View style={sidebar2.list}>
-            <MaterialCommunityIcons name='account-group' size={24} color='white' />
-            <View style={sidebar2.button}><Text style={sidebar2.buttonText}>CHATS</Text></View>
-            <View style={sidebar2.button}><Text style={sidebar2.buttonText}>STATUS</Text></View>
+            <TouchableOpacity style={sidebar2.button}><Text style={sidebar2.buttonText}>CHATS</Text></TouchableOpacity>
+            <TouchableOpacity style={sidebar2.button}><Text style={sidebar2.buttonText}>STATUS</Text></TouchableOpacity>
+            <TouchableOpacity style={sidebar2.button}><Text style={sidebar2.buttonText}>ABOUT</Text></TouchableOpacity>
           </View>
+        </View>
+      </View>
+
+      <View style={chats.container}>
+        <View style={chats.chat}>
+          <Image style={chats.avatar} source={{ uri: 'https://static.vecteezy.com/system/resources/previews/024/059/039/original/digital-art-of-a-cat-head-cartoon-with-sunglasses-illustration-of-a-feline-avatar-wearing-glasses-vector.jpg' }}></Image>
+          <Text style={chats.title}>João</Text>
+          <Text style={chats.subtitle}>Beleza!aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</Text>
+        </View>
+
+        <View style={chats.chat}>
+          <Image style={chats.avatar} source={{ uri: 'https://static.vecteezy.com/ti/vetor-gratis/p3/9292244-default-avatar-icon-vector-of-social-media-user-vetor.jpg' }}></Image>
+          <Text style={chats.title}>Maria</Text>
+          <Text style={chats.subtitle}>Oi, tudo bem?</Text>
         </View>
       </View>
     </View>
@@ -35,15 +50,56 @@ const styles = StyleSheet.create({
   },
 });
 
+const chats = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 128,
+    height: '100%',
+    width: '100%',
+    alignItems: 'center',
+    paddingTop: 16,
+  },
+  chat: {
+    position: 'relative',
+    width: '100%',
+    height: 64,
+    justifyContent: 'center',
+    borderBottomWidth: 0.3,
+  },
+  avatar: {
+    backgroundColor: 'blue',
+    width: 48,
+    height: 48,
+    position: 'absolute',
+    left: 12,
+    borderRadius: 50
+  },
+  title: {
+    position: 'absolute',
+    color: '#000',
+    fontSize: 25,
+    left: 70,
+    top: 8,
+  },
+  subtitle: {
+    position: 'absolute',
+    color: '#000',
+    fontSize: 20,
+    left: 72,
+    top: 30,
+    maxWidth: 256,
+  }
+});
+
 const sidebar = StyleSheet.create({
   container: {
     backgroundColor: '#01816A',
     alignItems: 'center',
     justifyContent: 'center',
     position: 'absolute',
-    top: 25,
+    top: 0,
     width: '100%',
-    height: 64
+    height: 128
   },
   title: {
     color: 'white',
@@ -69,7 +125,7 @@ const sidebar2 = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     position: 'absolute',
-    top: 64,
+    top: 80,
     width: '100%',
     height: 48
   },
@@ -79,6 +135,11 @@ const sidebar2 = StyleSheet.create({
     alignItems: 'center',
     flexDirection: 'row',
     gap: 8,
+  },
+  buttonSelected: {
+    marginBottom: -3,
+    borderBottomWidth: 3,
+    borderBottomColor: 'white'
   },
   button: {
     display: 'flex',
